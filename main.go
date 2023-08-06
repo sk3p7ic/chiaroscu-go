96396e09273972524e2f0d81916300584d37b006
Joshua Ibrom (sk3p7ic) <joshuaibrom@gmail.com>
2023-08-06 17:18:31 -0500

    Created methods to download datasets.
    Datasets are now downloaded from Google if not present on the local
    machine.

--- main.go
+++ main.go
@@ -4,7 +4,20 @@ import "fmt"
 
 import "github.com/sk3p7ic/chiaroscu-go/util"
 
+const (
+    DATASET_LOCAL_PATH = "datasets"
+    DATASET_URL = "https://storage.googleapis.com/cvdf-datasets/mnist/"
+    DATASET_TRAIN_IMAGES = "train-images-idx3-ubyte.gz"
+    DATASET_TRAIN_LABELS = "train-labels-idx1-ubyte.gz"
+    DATASET_TEST_IMAGES = "t10k-images-idx3-ubyte.gz"
+    DATASET_TEST_LABELS = "t10k-labels-idx1-ubyte.gz"
+)
+
 func main() {
     fmt.Println("Hello, World!")
-    util.LoadDataset()
+    var filenames = []string{
+        DATASET_TRAIN_IMAGES, DATASET_TRAIN_LABELS,
+        DATASET_TEST_IMAGES, DATASET_TEST_LABELS,
+    }
+    util.LoadDataset(DATASET_LOCAL_PATH, DATASET_URL, filenames)
 }
--- util/loader.go
+++ util/loader.go
@@ -1,7 +1,64 @@
 package util
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+    "io"
+    "net/http"
+    "github.com/schollz/progressbar/v3"
+)
 
-func LoadDataset() {
-    fmt.Println("Loading dataset...")
+func download_dataset_file(dataset_path, dataset_url, filename string) error {
+    fmt.Println("Downloading dataset...")
+    url := fmt.Sprintf("%s/%s", dataset_url, filename)
+    var dwnld_file_path = fmt.Sprintf("%s/%s.dwnld", dataset_path, filename)
+    req, _ := http.NewRequest("GET", url, nil)
+    resp, err := http.DefaultClient.Do(req)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+
+    f, _ := os.Create(dwnld_file_path)
+    
+    bar := progressbar.DefaultBytes(
+        resp.ContentLength,
+        fmt.Sprintf("Retrieving file from '%s'...\n\t", url),
+        )
+    io.Copy(io.MultiWriter(f, bar), resp.Body)
+    os.Rename(dwnld_file_path, fmt.Sprintf("%s/%s", dataset_path, filename))
+
+    return nil
+}
+
+func LoadDataset(dataset_path, fallback_url string, filenames []string) {
+	fmt.Printf("Checking for datasets ")
+    for i, dataset_name := range filenames {
+        fmt.Printf("'%s/%s'", dataset_path, dataset_name)
+        if i < len(filenames) - 1 {
+            fmt.Printf(", ")
+        } else {
+            fmt.Printf("...\n")
+        }
+    }
+	for _, dataset_name := range filenames {
+		_, err := os.Stat(fmt.Sprintf("%s/%s", dataset_path, dataset_name))
+		if os.IsNotExist(err) {
+			fmt.Println("[E] Dataset not found!")
+			fmt.Printf("    Downloading dataset '%s'...\n", dataset_name)
+            dl_err := download_dataset_file(dataset_path, fallback_url,
+                dataset_name)
+            if dl_err != nil {
+                fmt.Println("[E] An error occurred while downloading the dataset.")
+                fmt.Println(dl_err)
+                os.Exit(1)
+            }
+		} else if err != nil {
+			fmt.Println("[E] An error occurred while checking for the dataset.")
+			fmt.Println(err)
+			os.Exit(1)
+		} else {
+			fmt.Println("[I] Dataset found! Skipping download...")
+		}
+	}
 }
